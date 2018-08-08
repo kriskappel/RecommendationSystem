4e5848305d727d0556567475b7ebe44dfa3922f2
kriskappel <kristofer.kappel@gmail.com>
2018-08-08 04:13:40 +0000

    mudancas jorge

--- projeto/src/main/java/org/grouplens/lenskit/paperAdvisor/PaperAdvisor.java
+++ projeto/src/main/java/org/grouplens/lenskit/paperAdvisor/PaperAdvisor.java
@@ -26,56 +26,53 @@ import org.slf4j.LoggerFactory;
 import com.google.common.base.Throwables;
 import java.io.FileWriter;
 import java.sql.Timestamp;
-import java.sql.Timestamp;
 
 public class PaperAdvisor implements Runnable {
 
-	//main
-	public static void main(String[] args) throws IOException {
-        PaperAdvisor paperadvisor = new PaperAdvisor(args);
+    //main
+    public static void main(String[] args) throws IOException {
+       /* PaperAdvisor paperadvisor = new PaperAdvisor(args);
       
         paperadvisor.execRec(10L, 10L);
-
         //paperadvisor.run();
-        
-       //writeCSV(2,2,2);
+        */
+       Frame frame = new Frame();
+       frame.setVisible(true);
     }
 
-	//private variables
-	//private List<Long> userIds = new ArrayList<>();
+    //private variables
+    //private List<Long> userIds = new ArrayList<>();
     //private List<Long> userNRec = new ArrayList<>();
 
     private Long userId;
     private Long nRec;
 
-	private static final Logger printLogs = LoggerFactory.getLogger(PaperAdvisor.class);
+    private static final Logger printLogs = LoggerFactory.getLogger(PaperAdvisor.class);
 
     private Path path = Paths.get("data/movielens.yml");
 
     private File groovy = null;
     
 
-    private List<Long> popIds = new ArrayList<>();
-    private List<String> popTitle = new ArrayList();
+    static List<Long> popIds = new ArrayList<>();
+    static List<String> popTitle = new ArrayList();
 
-    private List<Long> itemIds = new ArrayList<>();
-    private List<String> itemTitle = new ArrayList();
+    static List<Long> itemIds = new ArrayList<>();
+    static List<String> itemTitle = new ArrayList();
 
-    private List<Long> userIds = new ArrayList<>();
-    private List<String> userTitle = new ArrayList();
+    static List<Long> userIds = new ArrayList<>();
+    static List<String> userTitle = new ArrayList();
 
     //recMethod:
     //1- item-item
     //2- user-user
     //3- Popularity rank
     private int recMethod;
-
-    public Boolean flag = true;
     
     //public methods
 
-    public PaperAdvisor(String[] args) {
-       // Boolean flag=true;
+    public PaperAdvisor() {
+       /* Boolean flag=true;
         int i = 0;
         for (String id: args) {
             if(i==0){
@@ -95,30 +92,7 @@ public class PaperAdvisor implements Runnable {
             }
 
 
-        }   
-        List<String> coldstart = new ArrayList();
-
-        for(Long j = 1L; j <11L; j++)
-        {
-            popIds.add(j);
-            itemIds.add(j);
-            userIds.add(j);
-        }
-
-        coldstart.add("Desenvolvendo um Sistema de Recomendacao de Objetos de Aprendizagem baseado em Competencias para a Educacao: relato de experiencias");
-        coldstart.add("Recommender systems in technology enhanced learning");
-        coldstart.add("A K-Means Clustering Algorithm");
-        coldstart.add("What is an Ontology?");
-        coldstart.add("The Role of Trust and Deception in Virtual Societies");
-        coldstart.add("Sharing a Secret: How Kerberos Works");
-        coldstart.add("Using trust in recommender systems: an experimental analysis");
-        coldstart.add("What drives a successful e-Learning? An empirical investigation of the critical factors influencing learner satisfaction");
-        coldstart.add("Constructionist Design Methodology for Interactive Intelligences");
-        coldstart.add("A knowledge and collaboration-based CBR process to improve network performance-related support activities");
-
-        popTitle = coldstart;
-        itemTitle = coldstart;
-        userTitle = coldstart;
+        }   */
 
                  
     }
@@ -151,8 +125,8 @@ public class PaperAdvisor implements Runnable {
 
     public void run() {
 
-     	//lenskit config
-     	LenskitConfiguration configLenskit = null;
+        //lenskit config
+        LenskitConfiguration configLenskit = null;
 
         //recMethod:
         //1- item-item
@@ -161,7 +135,7 @@ public class PaperAdvisor implements Runnable {
         File groovy = new File("etc/popularityRank.groovy");
         if(recMethod == 1){
 
-         	groovy = new File("etc/item-item.groovy");
+            groovy = new File("etc/item-item.groovy");
         }
         else if(recMethod == 2){
             groovy = new File("etc/user-user.groovy");
@@ -179,7 +153,7 @@ public class PaperAdvisor implements Runnable {
             throw new RuntimeException("LenskitConfiguration", e);
         }
 
-     	//get data from files
+        //get data from files
         DataAccessObject dao;
 
         try {
@@ -193,13 +167,13 @@ public class PaperAdvisor implements Runnable {
 
 
         LenskitRecommenderEngine engineLenskit = LenskitRecommenderEngine.build(configLenskit, dao);
-       	LenskitRecommender recommender = engineLenskit.createRecommender(dao);
+        LenskitRecommender recommender = engineLenskit.createRecommender(dao);
         printLogs.info("recommender created");
             
         ItemRecommender itemRec = recommender.getItemRecommender();
         assert itemRec != null; //recommender configured -> !=null
        
-       	//print recommendations
+        //print recommendations
 
         int idUser = userId.intValue();
         int numberRec = nRec.intValue();
@@ -209,69 +183,60 @@ public class PaperAdvisor implements Runnable {
         List<String> titles = new ArrayList<>();
         
         ResultList recommendations = itemRec.recommendWithDetails(idUser, numberRec, null, null);
-        System.out.println("\nPapers recommended to user  " +idUser+ " :\n");
-        try{
-            for (int i = 0 ; i < numberRec ; i++) { //recommend papers
+        //System.out.println("\nPapers recommended to user  " +idUser+ " :\n");
 
-            	Result paper = recommendations.get(i);
+        for (int i = 0 ; i < numberRec ; i++) { //recommend papers
 
-                Entity entityPaper = dao.lookupEntity(CommonTypes.ITEM, paper.getId());
-                String namePaper = "";
+            Result paper = recommendations.get(i);
 
-                if (entityPaper != null) {
-                    namePaper = entityPaper.maybeGet(CommonAttributes.NAME);
-                }
+            Entity entityPaper = dao.lookupEntity(CommonTypes.ITEM, paper.getId());
+            String namePaper = "";
 
-                //System.out.print("\t" + i + "- Title = " + namePaper + " | id = " + paper.getId() + " | score = " +paper.getScore());
-                //System.out.printf("%.2f\n", paper.getScore());
-                indices.add(paper.getId());
-                titles.add(namePaper);
-
-
-                System.out.format("\t %d - Title = %s | id = %d | score = %.2f\n", (i+1), namePaper, paper.getId(), paper.getScore());
-
+            if (entityPaper != null) {
+                namePaper = entityPaper.maybeGet(CommonAttributes.NAME);
             }
 
-            if(recMethod == 1){
-                itemIds = indices;
-                itemTitle = titles;
-            }
-            else if(recMethod == 2){
-                userIds = indices;
-                userTitle = titles;
-            }
-            else if(recMethod == 3){
-                popIds = indices;
-                popTitle = titles;
-            }
-            flag = true;
-        }
-        catch(Exception e)
-        {
-            this.flag = false;
+            //System.out.print("\t" + i + "- Title = " + namePaper + " | id = " + paper.getId() + " | score = " +paper.getScore());
+            //System.out.printf("%.2f\n", paper.getScore());
+            indices.add(paper.getId());
+            titles.add(namePaper);
+
+
+            //System.out.format("\t %d - Title = %s | id = %d | score = %.2f\n", (i+1), namePaper, paper.getId(), paper.getScore());
+
         }
 
+        if(recMethod == 1){
+            itemIds = indices;
+            itemTitle = titles;
+        }
+        else if(recMethod == 2){
+            userIds = indices;
+            userTitle = titles;
+        }
+        else if(recMethod == 3){
+            popIds = indices;
+            popTitle = titles;
+        }
+    
     }
     
     public static void writeCSV(int userID, int movieID, float rating) throws IOException{
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        FileWriter fw = new FileWriter("data/usuarios.csv",true);
-        StringBuilder sb = new StringBuilder();
-        
-        sb.append(userID);
-        sb.append(',');
-        sb.append(movieID);
-        sb.append(',');
-        sb.append(rating);
-        sb.append(',');
-        sb.append(timestamp.getTime());
-        sb.append('\n');
-        fw.append(sb.toString());
-        fw.close();
-        System.out.println("done!");
-    }
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    FileWriter fw = new FileWriter("data/usuarios.csv",true);
+    StringBuilder sb = new StringBuilder();
+    
+    sb.append(userID);
+    sb.append(',');
+    sb.append(movieID);
+    sb.append(',');
+    sb.append(rating);
+    sb.append(',');
+    sb.append(timestamp.getTime());
+    sb.append('\n');
+    fw.append(sb.toString());
+    fw.close();
+    System.out.println("done!");
+}
  
 }
-
-
-   
